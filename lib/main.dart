4d95a09768c8bd3c031a6c35b5ffe7800077f6d5
yein <inwon9892@naver.com>
2023-03-13 05:06:31 +0900

    feat: flutter project and firebase connect

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,13 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:google/Login/main_screen.dart';
 import 'package:google/Login/splash_screen.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await dotenv.load(fileName: ".env");
   // await FirebaseFirestore.instance.clearPersistence();
   runApp(MyApp());
