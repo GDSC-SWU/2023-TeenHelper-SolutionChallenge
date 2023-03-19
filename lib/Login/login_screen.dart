d91c2fa51af5e5f50cd41dc088efdc87f627d6e6
yein <inwon9892@naver.com>
2023-03-20 02:02:38 +0900

    feat: Walkthroughs page add

--- lib/Login/login_screen.dart
+++ lib/Login/login_screen.dart
@@ -3,6 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class login_screen extends StatelessWidget {
   final FirebaseAuth _auth = FirebaseAuth.instance;
@@ -43,31 +44,93 @@ class login_screen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final PageController controller = PageController(initialPage: 0,);
     return Scaffold(
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-            Container(
-                margin: EdgeInsets.fromLTRB(16.w,0,16.w,48.h),
-                child: InkWell(
-                  child: Image.asset('images/login_button.png'),
-                  onTap: () => {
-                    google_SignIn(),
+            Center(
+                  child: SizedBox(
+                    width: ScreenUtil().setWidth(272),
+                    height: ScreenUtil().setHeight(660),
+                    child: PageView(
+                    controller: controller,
+                    children: [
+                      Column(
+                        children: [
+                          SizedBox(width: ScreenUtil().setWidth(272), height: ScreenUtil().setHeight(154)),
+                          Image.asset('images/login_image_complete1.png', width: ScreenUtil().setWidth(272), height: ScreenUtil().setHeight(272)),
+                          SizedBox(width: ScreenUtil().setWidth(272), height: ScreenUtil().setHeight(64)),
+                          Image.asset('images/login_text_complete1.png'),
+                    ]
+                      ),
+                      Column(
+                          children: [
+                            SizedBox(width: ScreenUtil().setWidth(272), height: ScreenUtil().setHeight(154)),
+                            Image.asset('images/login_image_complete2.png', width: ScreenUtil().setWidth(272), height: ScreenUtil().setHeight(272)),
+                            SizedBox(width: ScreenUtil().setWidth(272), height: ScreenUtil().setHeight(64)),
+                            Image.asset('images/login_text_complete2.png'),
+                          ]
+                      ),
+                      Column(
+                          children: [
+                            SizedBox(width: ScreenUtil().setWidth(272), height: ScreenUtil().setHeight(154)),
+                            Image.asset('images/login_image_complete3.png', width: ScreenUtil().setWidth(272), height: ScreenUtil().setHeight(272)),
+                            SizedBox(width: ScreenUtil().setWidth(272), height: ScreenUtil().setHeight(64)),
+                            Image.asset('images/login_text_complete3.png'),
+                          ]
+                      ),
+                      Column(
+                          children: [
+                            SizedBox(width: ScreenUtil().setWidth(272), height: ScreenUtil().setHeight(154)),
+                            Image.asset('images/login_image_complete4.png', width: ScreenUtil().setWidth(272), height: ScreenUtil().setHeight(272)),
+                            SizedBox(width: ScreenUtil().setWidth(272), height: ScreenUtil().setHeight(64)),
+                            Image.asset('images/login_text_complete4.png'),
+                          ]
+                      ),
+                    ],
+                ),
+                  )
+            ),
+            SmoothPageIndicator(
+              controller: controller,
+              count:  4,
+              axisDirection: Axis.horizontal,
+              effect:  SlideEffect(
+                  spacing:  8.0,
+                  radius:  4.0,
+                  dotWidth:  8.0,
+                  dotHeight:  8.0,
+                  paintStyle:  PaintingStyle.fill,
+                  strokeWidth:  1.5,
+                  dotColor:  Color(0xFFE9E9E9),
+                  activeDotColor: Color(0xFFE76D3B)
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                  margin: EdgeInsets.fromLTRB(ScreenUtil().setWidth(16),ScreenUtil().setHeight(14),ScreenUtil().setWidth(16),ScreenUtil().setHeight(0)),
+                  child: InkWell(
+                    child: Image.asset('images/login_button.png'),
+                    onTap: () => {
+                      google_SignIn(),
 
-                  },
-                )
-                /*SizedBox(
-                  width: double.infinity,
-                  height: 96.h,
-                  child: IconButton(
-                    onPressed: () {
-                      google_SignIn();
-                      },
-                    icon: Image.asset('images/login_button.png'),
-                    constraints: const BoxConstraints(),
-                  ),
-                )*/
+                    },
+                  )
+                  /*SizedBox(
+                    width: double.infinity,
+                    height: 96.h,
+                    child: IconButton(
+                      onPressed: () {
+                        google_SignIn();
+                        },
+                      icon: Image.asset('images/login_button.png'),
+                      constraints: const BoxConstraints(),
+                    ),
+                  )*/
+              ),
             ),
           ],
         ),
