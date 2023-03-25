071e5c234d14acd32c338bfa32b5b36cc479b63b
jiyun B <jiyunbae36@gmail.com>
2023-03-26 03:37:44 +0900

    feat: add Notification and Crawling

---
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/jiyunbae/Project/Bridge/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/jiyunbae/Project/notification/notificataion"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/jiyunbae/Project/notification/notificataion/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/jiyunbae/Project/notification/notificataion/.dart_tool/package_config.json"
